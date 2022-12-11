be7ca98b34f2bcc0c24a38ad4eb459a94fe59133
naoto7010code <n.m.1210@au.com>
2022-12-11 11:52:27 +0900

    本番環境修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,7 +8,7 @@ class OrderForm
     validates :item_id
     # paymentモデルのバリデーション
     validates :post_code,    format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :area_id,      numericality: { other_than: 0, message: "can't be blank" }
+    validates :area_id,      numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
