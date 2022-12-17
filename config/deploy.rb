a31b8fde2bf7a62d7614aafbbcbe3849f612f829
naoto7010code <n.m.1210@au.com>
2022-12-17 17:22:58 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock '3.17.1'
 
 set :application, "furima-38452"
 set :repo_url, "git@github.com:naoto7010code/furima-38452.git"
